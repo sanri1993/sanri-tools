ea564e1be4732905ba24cf29cbbc11f94e649926
9420 <ningxiangsanri@163.com>
2020-07-22 21:51:33 +0800

    没想到好的解决方案, redis 有集群和非集群两种,但先要通过 jedis 获取是否为集群

---
+++ tools-redis/src/main/java/com/sanri/tools/modules/redis/dto/RedisNode.java
@@ -0,0 +1,16 @@
+package com.sanri.tools.modules.redis.dto;
+
+import lombok.Data;
+import redis.clients.jedis.HostAndPort;
+
+@Data
+public class RedisNode {
+    private String id;
+    private HostAndPort hostAndPort;
+    private String role;
+    private String master;
+
+    // 槽位列表,只有集群模式才会有
+    private int slotStart;
+    private int slotEnd;
+}
