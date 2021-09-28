213f55707ca148b76a29508ff1d94073ef8cecc6
huangzhr <ningxiangsanri@163.com>
2021-09-28 22:10:36 +0800

    patchcode

---
+++ tools-patchcode/src/main/java/com/sanri/tools/modules/codepatch/controller/GitController.java
@@ -0,0 +1,10 @@
+package com.sanri.tools.modules.codepatch.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/git")
+public class GitController {
+
+}
---
+++ tools-patchcode/src/main/java/com/sanri/tools/modules/codepatch/service/GitService.java
@@ -0,0 +1,8 @@
+package com.sanri.tools.modules.codepatch.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GitService {
+
+}
