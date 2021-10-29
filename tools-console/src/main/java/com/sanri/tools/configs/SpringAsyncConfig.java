2fd24b1729e4c93a7fd43533a5f103cda62660b1
huangzhr <ningxiangsanri@163.com>
2021-10-29 17:20:37 +0800

    添加异步任务的上下文数据

--- tools-console/src/main/java/com/sanri/tools/configs/SpringAsyncConfig.java
+++ tools-console/src/main/java/com/sanri/tools/configs/SpringAsyncConfig.java
@@ -1,11 +1,16 @@
 package com.sanri.tools.configs;
 
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 @Configuration
@@ -21,8 +26,36 @@ public class SpringAsyncConfig implements AsyncConfigurer {
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.setBeanName("asyncExecutor");
         executor.setTaskDecorator(new AsyncTaskDecorator());
-        executor.initialize();
+//        executor.initialize();
 
         return executor;
+    }
+
+    /**
+     * 对于异步任务时, 同样也能获取到 TraceId
+     * spring 的异步任务 @Async
+     */
+    public static class AsyncTaskDecorator implements TaskDecorator {
+        @Override
+        public Runnable decorate(Runnable runnable) {
+            try {
+                RequestAttributes context = RequestContextHolder.currentRequestAttributes();
+                Map<String,String> previous = MDC.getCopyOfContextMap();
+                return () -> {
+                    try {
+                        RequestContextHolder.setRequestAttributes(context);
+
+                        MDC.setContextMap(previous);
+
+                        runnable.run();
+                    } finally {
+                        RequestContextHolder.resetRequestAttributes();
+                        MDC.clear();
+                    }
+                };
+            } catch (IllegalStateException e) {
+                return runnable;
+            }
+        }
     }
 }
