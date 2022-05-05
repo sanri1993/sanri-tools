5117526d83d6fc02cf33328b7d9e61926000f0f0
huangzhr <ningxiangsanri@163.com>
2022-05-05 16:29:24 +0800

    解决一些问题

--- tools-websocket/src/main/java/com/sanri/tools/modules/websocket/configs/IpHandshakeInterceptor.java
+++ tools-websocket/src/main/java/com/sanri/tools/modules/websocket/configs/IpHandshakeInterceptor.java
@@ -2,6 +2,7 @@ package com.sanri.tools.modules.websocket.configs;
 
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
@@ -12,6 +13,8 @@ import java.util.Map;
 public class IpHandshakeInterceptor implements HandshakeInterceptor {
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        // 使用 tomcat 时, 需要确保 async-supported 开启
+        ((ServletServerHttpRequest) request).getServletRequest().setAttribute("org.apache.catalina.ASYNC_SUPPORTED", true);
         attributes.put("ip", request.getRemoteAddress());
         return true;
     }
